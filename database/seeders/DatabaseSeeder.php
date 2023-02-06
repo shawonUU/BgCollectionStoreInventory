7d0cd045f55689e197afe34e5c1c6592e2ae5710
devsozib <mdsajibhassa01993@gmail.com>
2023-02-06 10:45:33 +0600

    calender status seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
+            CalenderStatusSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory()->create([
